fb73d9b2860dd94a70107f9a8f6887a3e252eb40
NitipoomP <few12345xxxx@gmail.com>
2023-01-15 14:08:03 +0700

    first commit

---
+++ ProfileApp/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def test(request):
+    return HttpResponse("<H1> Hello World </H1>")
+
+def firstWeb(request):
+    return render(request,"FirstWeb.html")
+
+def secondpage (request):
+    return render(request,'secondpage.html')
+
+def thridpage (request):
+    return render(request,'thridpage.html')
